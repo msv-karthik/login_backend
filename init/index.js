067b4b8cd968b55727eeb5df5895e978014f8002
msv-karthik <msvkarthik12@gmail.com>
2025-01-02 20:48:58 +0530

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/login_be");
+
+connect.then(()=>{
+    console.log("Database connected successfully");
+})
+.catch(()=>{
+    console.log("Database cannot be connected");
+});
+
+const LoginSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+});
+
+const collection = new mongoose.model("users",LoginSchema);
+
+module.exports = collection;
